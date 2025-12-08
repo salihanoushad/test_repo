23e1426cd949aa12545530be100a3319a47261b4
salihanoushad <salihanoushad9@gmail.com>
2025-12-08 14:17:52 +0530

    Add files via upload

---
+++ fib2.py
@@ -0,0 +1,9 @@
+def fib(n,memo={}):
+    if n in memo:
+        return memo[n]
+    if n<=1:
+        return n
+    memo[n]=fib(n-1,memo)+fib(n-2,memo)
+    return memo[n]
+n=int(input("enter the values of n:"))
+print(f"the{n}th fibonacci number is: {fib(n)}")
